90e74b8c1598c2378f25510a5d36245a147271e5
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-08 08:56:00 -0300

    [Alura] Finish class two
    - Validations

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -5,5 +5,28 @@ var titulo = document.querySelector(".titulo");
 console.log(titulo);
 console.log(titulo.textContent);
 
-titulo.textContent = "Conteúdo alterado via javascript";
-console.log(titulo);
+var paciente = document.querySelector("#primeiro-paciente");
+var peso     = paciente.querySelector(".info-peso").textContent;
+var altura   = paciente.querySelector(".info-altura").textContent;
+var imc      = paciente.querySelector(".info-imc");
+
+var pesoEhValido   = true;
+var alturaEhValida = true;
+
+if (peso < 1 || peso > 999) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    imc.textContent = "Peso inválido!";
+}
+
+if (altura < 1 || altura > 2.99) {
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    imc.textContent = "Altura inválida!";
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imcCalc  = peso / (Math.pow(altura, 2));
+    imc.textContent = imcCalc;
+}
+
