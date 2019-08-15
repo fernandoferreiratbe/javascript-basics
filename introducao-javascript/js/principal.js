b6efe375fadb748607dbdee49d0f92401e31baf1
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-15 08:33:12 -0300

    [Alura-JS] Implement validation for pacientes
    - Get all pacientes using querySelectorAll function
    - Validate each paciente through for looping

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -5,28 +5,34 @@ var titulo = document.querySelector(".titulo");
 console.log(titulo);
 console.log(titulo.textContent);
 
-var paciente = document.querySelector("#primeiro-paciente");
-var peso     = paciente.querySelector(".info-peso").textContent;
-var altura   = paciente.querySelector(".info-altura").textContent;
-var imc      = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
 
-var pesoEhValido   = true;
-var alturaEhValida = true;
+for (var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-if (peso < 1 || peso > 999) {
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    imc.textContent = "Peso inválido!";
+    var peso     = paciente.querySelector(".info-peso").textContent;
+    var altura   = paciente.querySelector(".info-altura").textContent;
+    var imc      = paciente.querySelector(".info-imc");
+    
+    var pesoEhValido   = true;
+    var alturaEhValida = true;
+    
+    if (peso < 1 || peso > 999) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        imc.textContent = "Peso inválido!";
+    }
+    
+    if (altura < 1 || altura > 2.99) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        imc.textContent = "Altura inválida!";
+    }
+    
+    if (pesoEhValido && alturaEhValida) {
+        var imcCalc  = peso / (Math.pow(altura, 2));
+        imc.textContent = imcCalc.toFixed(2);
+    }
 }
 
-if (altura < 1 || altura > 2.99) {
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    imc.textContent = "Altura inválida!";
-}
-
-if (pesoEhValido && alturaEhValida) {
-    var imcCalc  = peso / (Math.pow(altura, 2));
-    imc.textContent = imcCalc;
-}
 
