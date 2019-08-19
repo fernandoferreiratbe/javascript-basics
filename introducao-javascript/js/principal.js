d72c59c31df569e5e814c89f248172c59ca72559
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-19 09:42:56 -0300

    [Alura-JS] Create elements with javascript
    - Get elements from web page
    - Access elements from form element
    - Create elements with createElement function
    - Append elements to parent with appendChild function

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,14 +1,9 @@
 console.log("Fui carregado de um arquivo externo.");
 
 var titulo = document.querySelector(".titulo");
-
 titulo.addEventListener("click", function() {
     console.log("Implement first anonymous function.");
 });
-
-function mostraMensagem() {
-    console.log("Hello, I have been clicked.");
-}
 
 var pacientes = document.querySelectorAll(".paciente");
 
@@ -45,5 +40,32 @@ for (var i = 0; i < pacientes.length; i++) {
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log("Hello, I'm a button and I has been clicked.");
+    
+    var form = document.querySelector("#form-adiciona");
+
+    var nome    = form.nome.value;
+    var peso    = form.peso.value;
+    var altura  = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var pacienteTr = document.createElement("tr");
+    
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
 });
