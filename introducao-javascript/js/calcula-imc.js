743b0af26af769abd76d8c38c6dbee78bb38a3b0
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-20 14:37:59 -0300

    [Alura-JS] Extract calculaImc function
    - Extract algorithm to calculate imc value
    - Refactor source code in order to apply javascript best practices

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -32,40 +32,12 @@ for (var i = 0; i < pacientes.length; i++) {
     }
     
     if (pesoEhValido && alturaEhValida) {
-        var imcCalc  = peso / (Math.pow(altura, 2));
-        imc.textContent = imcCalc.toFixed(2);
+        imc.textContent = calculaImc(peso, altura);
     }
 }
 
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-    
-    var form = document.querySelector("#form-adiciona");
+function calculaImc(peso, altura) {
+    var imc = peso / (Math.pow(altura, 2));
 
-    var nome    = form.nome.value;
-    var peso    = form.peso.value;
-    var altura  = form.altura.value;
-    var gordura = form.gordura.value;
-
-    var pacienteTr = document.createElement("tr");
-    
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
-});
+    return imc.toFixed(2);
+}
