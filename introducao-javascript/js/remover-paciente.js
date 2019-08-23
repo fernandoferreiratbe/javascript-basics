a929e3a42779e0ae69b93398184d8cae92370cf7
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-23 08:47:04 -0300

    [Alura JS] Refactor source code
    - Refactor source code regarding to remove table elements operation

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -2,8 +2,5 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function(event) {
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode;
-
-    paiDoAlvo.remove();
+    event.target.parentNode.remove();
 })
