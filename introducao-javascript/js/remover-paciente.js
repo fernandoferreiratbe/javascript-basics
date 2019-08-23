3eda6ee6ea181343f1e8f01c48537fba12b94eed
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-23 08:40:07 -0300

    [Alura JS] Implement event bubbling
    - Instead of create an event listener for each table row, we create an
    event to parent, in this case table, and it can listen all children.
    - In this case, once a table data is cliecked twice, we can access its
    parent and then remove it.

--- introducao-javascript/js/remover-paciente.js
+++ introducao-javascript/js/remover-paciente.js
@@ -1,8 +1,9 @@
 var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
 
-pacientes.forEach(function(paciente) {
-    paciente.addEventListener("dblclick", function() {
-        console.log("Fui clicado duas vezes.");
-        this.remove();
-    });
-});
+tabela.addEventListener("dblclick", function(event) {
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+
+    paiDoAlvo.remove();
+})
