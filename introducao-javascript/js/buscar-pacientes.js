dc6c04ec2b11c8ea32e05f9076bd9219286f15dd
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-26 13:18:11 -0300

    [Alura JS] Parse string to an array of json

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -4,9 +4,15 @@ buscarPacientes.addEventListener("click", function() {
 
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
-    
+
     xhr.addEventListener("load", function() {
-        console.log(xhr.responseText);
+        var resposta = xhr.responseText;
+        console.log(resposta);
+        console.log(typeof resposta);
+
+        var pacientes = JSON.parse(resposta);
+        console.log(pacientes);
+        console.log(typeof pacientes);
     });
 
     xhr.send();
