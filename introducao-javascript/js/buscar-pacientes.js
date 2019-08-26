0d5f0a275ee945a3f9e6f91416d71f3fc7137c4b
Fernando Ferreira <fernando.ferreira.tbe@gmail.com>
2019-08-26 13:51:54 -0300

    [Alura JS] Deal with request error

--- introducao-javascript/js/buscar-pacientes.js
+++ introducao-javascript/js/buscar-pacientes.js
@@ -1,17 +1,25 @@
 var buscarPacientes = document.querySelector("#buscar-pacientes");
-buscarPacientes.addEventListener("click", function() {
+buscarPacientes.addEventListener("click", function () {
     console.log("Buscando pacientes...");
 
+    var erroAjax = document.querySelector("#erro-ajax");
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
-    xhr.addEventListener("load", function() {
-        var resposta = xhr.responseText;
-        var pacientes = JSON.parse(resposta);
+    xhr.addEventListener("load", function () {
+        if (xhr.status === 200) {
+            erroAjax.classList.add("invisivel");
+            var resposta = xhr.responseText;
+            var pacientes = JSON.parse(resposta);
 
-        pacientes.forEach(function(paciente) {
-            adicionaPacienteNaTabela(paciente);
-        });
+            pacientes.forEach(function (paciente) {
+                adicionaPacienteNaTabela(paciente);
+            });
+        } else {
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            erroAjax.classList.remove("invisivel");
+        }
     });
 
     xhr.send();
